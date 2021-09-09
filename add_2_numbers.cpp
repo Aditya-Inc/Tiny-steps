2ebdcf952f7e3c19baedecc9340d35db6b4c0008
Aditya <79736755+Aditya-Inc@users.noreply.github.com>
2021-09-09 22:14:54 +0530

    new file addition

---
+++ add_2_numbers.cpp
@@ -0,0 +1,18 @@
+//Cpp program to add to digits
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    
+    cout << "Enter two integers:\n ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << a << " + " <<  b << " = " << sum;     
+
+    return 0;
+}

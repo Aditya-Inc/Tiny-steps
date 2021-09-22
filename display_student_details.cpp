02b3768d66cfce382551d9f38088d5c4653afb05
Aditya <79736755+Aditya-Inc@users.noreply.github.com>
2021-09-22 14:07:02 +0530

    new files

---
+++ display_student_details.cpp
@@ -0,0 +1,44 @@
+//program to accept and dispaly students details
+
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+struct student
+{
+       char sname[20];
+       int rollno;
+       int French, maths, science;
+};
+int main()
+{
+       struct student st[100];
+       int n, i;
+       cout<<"\n How Many Students You Want to Enter? : ";
+       cin>>n;
+       for(i=0; i<=n-1; i++)
+       {
+              cout<<"\n Student Name :  ";
+              cin>>st[i].sname;
+              cout<<"\n Roll Number  :  ";
+              cin>>st[i].rollno;
+              cout<<"\n Marks of Three Subjects ";
+              cout<<"\n ---------------------";
+              cout<<"\n French      :  ";
+              cin>>st[i].French;
+              cout<<"\n Maths        :  ";
+              cin>>st[i].maths;
+              cout<<"\n Science      :  ";
+              cin>>st[i].science;
+              cout<<"\n ---------------------------------";
+       }
+       cout<<"\n\n\t ********** Displaying Student's Details ********** \n\n"<<" ";
+       cout<<"\n\tStudent's Name"<<"\tRoll Number"<<"\tMarks of Three Subjects";
+       cout<<"\n\n\t\t\t\t\tFrench  Maths  Science";
+       for(i=0;i<=n-1;i++)
+       {
+              cout<<"\n\t----------------------------------------------------------";
+              cout<<"\n\t "<<st[i].sname<<"\t "<<st[i].rollno<<"\t\t "<<st[i].French<<"\t "<<st[i].maths<<"\t "<<st[i].science;
+       }
+       return 0;
+}
---
+++ restraurent_bill.cpp
@@ -0,0 +1,55 @@
+//Write a C++ program to accept bill no, customer code, name and amount of 100 bills and print the details of the customer with highest billed amount.
+
+#include<iostream>
+using namespace std;
+
+struct customer
+{
+     int billno;
+     int custcode;
+     char custname[20];
+     float amount;
+};
+int main()
+{
+     struct customer c[3];
+     int i, max;
+
+     for(i=0;i<3;i++)
+     {
+          cout<<"\n Enter Bill No.      :  ";
+          cin>>c[i].billno;
+          cout<<"\n Enter Customer Code :  ";
+          cin>>c[i].custcode;
+          cout<<"\n Enter Customer Name :  ";
+          cin>>c[i].custname;
+          cout<<"\n Enter Amount        :  ";
+          cin>>c[i].amount;
+          cout<<"\n -----------------------------";
+     }
+     for(i=0;i<3;i++)
+     {
+          cout<<"\n Bill No.        :  "<<c[i].billno;
+          cout<<"\n Customer Code   :  "<<c[i].custcode;
+          cout<<"\n Customer Name   :  "<<c[i].custname;
+          cout<<"\n Amount          :  "<<c[i].amount<<endl;
+     }
+     max=c[i].amount;
+     for(i=0;i<3;i++)
+     {
+          if(c[i].amount>max)
+          {
+               max=c[i].amount;
+          }
+     }
+     for(i=0;i<3;i++)
+     {
+          if(c[i].amount==max)
+          {
+               cout<<"\n ------------------------------------------------\n";
+               cout<<"\n Highest billed amount : "<<c[i].custname;
+               cout<<"\n Customer name          : "<<c[i].amount;
+          }
+     }
+     return 0;
+}
